e3da6bd65375b8af18bec9795e4879bf69a599f2
Khushi Sharma <87928896+Khushi21S@users.noreply.github.com>
2025-05-16 17:49:15 +0530

    updated function working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function submit(){
     const button = document.getElementById("submit");
     button.addEventListener('click', function(){
-        alert("Got it");
+        alert("Working on it");
     })
 };
 
