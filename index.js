2288637953d04dad2c6440cfcbc4e3383f594a5f
Khushi Sharma <87928896+Khushi21S@users.noreply.github.com>
2025-06-11 03:10:49 +0530

    added alerts

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const listItems = document.getElementById("taskList");
 const onGoingTasks = document.getElementById("onGoingTasks");
 const completedTasks = document.getElementById("completedTasks");
 
-
 //function to add a task in the list
 
-function createTask(event, inputField, id){
+function createTask(event, inputField, id) {
   event.preventDefault();
+  if (inputField == "" || inputField.value == "") {
+    alert("Please enter the Task name");
+    return;
+  }
   let listItem = document.createElement("li");
 
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  listItem.innerHTML = inputField.value ? inputField.value :inputField;
+  listItem.innerHTML = inputField.value ? inputField.value : inputField;
   listItem.appendChild(checkbox);
 
   document.getElementById(`${id}`).appendChild(listItem);
   inputField.value = "";
 }
+
 
 // function to add task in the listItems.
 function addTask(event) {
@@ -29,46 +33,54 @@ function addTask(event) {
 
 //function to delete task
 
-function deleteTask(list){
+function deleteTask(list) {
+  if(list.childNodes === 0) alert("Please add a task first");
   const selectedTasks = Array.from(list.childNodes);
   selectedTasks.forEach((element) => {
     const node = element.querySelector('input[type="checkbox"]');
-    if(node && node.checked){
+    if (node && node.checked) {
       element.remove();
+    } else{
+      alert("Please select a task");
     }
-  })
+  });
 }
 
-function deleteTaskFromTaskList(){
+function deleteTaskFromTaskList() {
   deleteTask(listItems);
 }
 
-function deleteTaskFromCompletedList(){
+function deleteTaskFromCompletedList() {
   deleteTask(completedTasks);
 }
 
 //function to add task to any list
-function addTaskToAnyList(event, addFromList, addToList ){
+function addTaskToAnyList(event, addFromList, addToList) {
   const selectedTasks = Array.from(addFromList.childNodes);
   selectedTasks.forEach((element) => {
     const node = element.querySelector('input[type = "checkbox"]');
-      if(node && node.checked){
-         createTask(event, element.innerText, `${addToList}`);
-         element.remove(); 
-         //listItems.removeChild(element) this can also be used
-      }
+    if (node && node.checked) {
+      createTask(event, element.innerText, `${addToList}`);
+      element.remove();
+      //listItems.removeChild(element) this can also be used
+    } else{
+      alert("Please select a task");
+    }
   });
 }
 
+//adding event listener to form to submit task
+form.addEventListener("submit", addTask);
+
 //function to remove tasks from tasklist and add to onGoingTasks
 
-function addTasktoOngoingTasks(event){
+function addTasktoOngoingTasks(event) {
   addTaskToAnyList(event, listItems, "onGoingTasks");
 }
 
 //function to remove tasks from onGoingTasks and add to completed task
-function addTasktoCompletedTasks(event){
-  addTaskToAnyList(event, onGoingTasks, "completedTasks")
+function addTasktoCompletedTasks(event) {
+  addTaskToAnyList(event, onGoingTasks, "completedTasks");
 }
 
 //adding event listener to move Button to Ongoing task
@@ -76,9 +88,8 @@ const moveButtonToOngoingTask = document.getElementById("button1");
 moveButtonToOngoingTask.addEventListener("click", addTasktoOngoingTasks);
 
 //adding event listener to move button to Completed task
-const moveButtonToCompletedTask = document.getElementById("button3")
+const moveButtonToCompletedTask = document.getElementById("button3");
 moveButtonToCompletedTask.addEventListener("click", addTasktoCompletedTasks);
-
 
 //adding event listener to remove task from listItems
 const deleteButtonFromTaskList = document.getElementById("button2");
@@ -86,12 +97,12 @@ deleteButtonFromTaskList.addEventListener("click", deleteTaskFromTaskList);
 
 //adding event listener to remove task from completedTask
 const deleteButtonFromCompletedTask = document.getElementById("button4");
-deleteButtonFromCompletedTask.addEventListener("click", deleteTaskFromCompletedList);
+deleteButtonFromCompletedTask.addEventListener(
+  "click",
+  deleteTaskFromCompletedList
+);
 
 
-
-//adding event listener to form to submit task
-form.addEventListener("submit", addTask);
 
 //function to add strikethrough to the list items.
 listItems.addEventListener("click", function (event) {
@@ -104,6 +115,8 @@ listItems.addEventListener("click", function (event) {
     }
   }
 });
+
+
 
 //showing current date
 const date = new Date().toLocaleDateString();
